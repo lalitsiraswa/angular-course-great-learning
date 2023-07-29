d95b12bc828a70eaded7cad3ae0f81ff292f1a64
lalit <lalit@lalits-MacBook-Pro.local>
2023-07-29 12:48:20 +0530

    Added Room Interface

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent {
-
   hotelName: string = "Hilton Hotel";
   numberOfRooms: number = 10;
   hideRooms: boolean = false;
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
   toggle(): void {
     this.hideRooms = !this.hideRooms;
   }
