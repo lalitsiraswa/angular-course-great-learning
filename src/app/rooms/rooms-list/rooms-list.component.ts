4ce817957ac5cb0313ddd8419f6e0b8ed313cfa9
lalit <lalit@lalits-MacBook-Pro.local>
2023-07-30 22:27:08 +0530

    Sharing data between child and parent components and @Input() Directive

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { RoomList } from '../rooms';
+
+@Component({
+  selector: 'hinv-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss']
+})
+export class RoomsListComponent {
+  // It will get the Input/Data from parent.
+  // What it will Do: It will make 'rooms' property as an valid HTML property on this ('hinv-rooms-list')
+  // HTML Element, So will be able to do Property Binding. 
+  @Input() rooms: RoomList[] = [];
+}
